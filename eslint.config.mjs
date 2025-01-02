a4295abe80d5c8b9a11637ce8551f42963217a40
prospal-zzZ <zabj.kart@gmail.com>
2025-01-02 23:38:55 +0400

    commit-18

--- eslint.config.mjs
+++ eslint.config.mjs
@@ -14,7 +14,7 @@ const eslintConfig = [
       extends: ['next'],
       rules: {
         'react/no-unescaped-entities': 'off',
-        '@next/next/no-page-custom-font': 'off',
+        '@next/next/google-font-display': 'warn',
       },
     }),
   ]
